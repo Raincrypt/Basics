7e192b0d4b6bbd224d5e33b0ea34fe32ea4092c8
Mousom Sarkar <81576322+Raincrypt@users.noreply.github.com>
2022-12-28 11:50:35 +0530

    Update reverse_string.cpp

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -1,5 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+/*
+void reverseStr(string& str)
+{
+    int n = str.length();
+ 
+    // Swap character starting from two corners
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
+*/
+
 int main()
 {
     string s = "GeeksforGeeks";
