11a27968d0652fab0dfa9b97d0265427640f92ba
Mousom Sarkar <81576322+Raincrypt@users.noreply.github.com>
2023-01-02 19:07:50 +0530

    Update selectionSort.cpp

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int main{
+void selectionSort(int arr[], int n){
   
+}
+
+int main{
+  int arr[] = {5, 6, 2, 9, 7, 1};
+    cout<<"Sort {5, 6, 2, 9, 7, 1}: "<<endl;
+    selectionSort(arr, 0, 5);
+    printArray(arr);
   return 0;
 }
