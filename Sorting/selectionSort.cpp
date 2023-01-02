8d026186cfe0ec0158f7930e8b500effdec52f89
Mousom Sarkar <81576322+Raincrypt@users.noreply.github.com>
2023-01-02 19:27:02 +0530

    Update selectionSort.cpp

--- Sorting/selectionSort.cpp
+++ Sorting/selectionSort.cpp
@@ -8,13 +8,22 @@ void printArray(int arr[]){
 }
 
 void selectionSort(int arr[], int n){
-  
+  int temp = 0;
+  for(int i = 0; i < n - 1; i++){
+    for(int j = i + 1; j < n; j++){
+        if(arr[i] > arr[j]){
+            temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+        }
+    }
+  }
 }
 
 int main{
   int arr[] = {5, 6, 2, 9, 7, 1};
     cout<<"Sort {5, 6, 2, 9, 7, 1}: "<<endl;
-    selectionSort(arr, 0, 5);
+    selectionSort(arr, 0, 6);
     printArray(arr);
   return 0;
 }
