5dd8b290f59094096fc396189e0d63dc33dd1d99
Mousom Sarkar <81576322+Raincrypt@users.noreply.github.com>
2023-07-30 12:07:13 +0530

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int* sort(int arr[], int size){
+    cout << "Sorted Array: ";
     for(int i = 0; i < size; i++){
         for(int j = i + 1; j < size; j++){
             if(arr[i] > arr[j]){
@@ -12,22 +13,47 @@ int* sort(int arr[], int size){
                 arr[j] = temp;
             }
         }
-    }
-    return arr;
-}
-
-void binarySearch(int arr[], int size, int target){
-    sort(arr, size);
-    for(int i = 0; i < size; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
+    return arr;
+}
+
+int binarySearch(int arr[], int size, int target){
+    sort(arr, size);
+    int left = 0;
+    int right = size - 1;
+    
+    while(left < right){
+        int mid = (left + right) / 2;
+        if(arr[mid]!=target){
+            if(target > arr[mid]){
+                left = mid;
+                mid = (left + right)/2 + 1;
+                cout << "element should be between: "<< arr[left] << " and " << arr[right] << endl;
+                if(target == arr[mid]) return mid;
+                left+=1;
+            }
+            else if(target < arr[mid]){
+                right = mid;
+                mid = (left + right)/2 + 1;
+                cout << "element should be between: "<< arr[left] << " and " << arr[right] << endl;
+                if(target == arr[mid]) return mid;
+                right-=1;
+            }
+            else return -1;
+        }
+        else return 1;
+    }
+    return -1;
 }
 
 int main() {
     int n = 10;
-    int arr[n] = {4, 5, 2, 7, 10, 33, 22, 99, 0, 100};
-    int target = 22;
-    binarySearch(arr, n, target);
+    int arr[n] = {4, 5, 2, 7, 10, 33, 22, 99, 0, 110};
+    int target = 23;
+    int result = binarySearch(arr, n, target);
+    if(result == -1) cout<<"Doesn't exist" << endl;
+    else cout << target << " exists in the array." << endl;
     return 0;
 }
