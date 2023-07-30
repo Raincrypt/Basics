bdc61928ecc60f4892dba8dd4a26c89827e6d406
Mousom Sarkar <81576322+Raincrypt@users.noreply.github.com>
2023-07-30 11:07:30 +0530

    Update search.cpp

--- search.cpp
+++ search.cpp
@@ -1,3 +1,33 @@
 // Always Applied to a sorted array
 
+#include <iostream>
+using namespace std;
 
+int* sort(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        for(int j = i + 1; j < size; j++){
+            if(arr[i] > arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+void binarySearch(int arr[], int size, int target){
+    sort(arr, size);
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 10;
+    int arr[n] = {4, 5, 2, 7, 10, 33, 22, 99, 0, 100};
+    int target = 22;
+    binarySearch(arr, n, target);
+    return 0;
+}
